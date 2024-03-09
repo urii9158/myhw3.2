6e2f0b8091bca54e7c08b17b3f4404d0bde28d85
uri <katyacitti@gmail.com>
2024-03-09 17:02:29 +0200

    initial project code

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public int calculate(int weightInKg, double heightInMeters) {
+        double bmi = weightInKg / heightInMeters / heightInMeters;
+        return (int) bmi;
+    }
+}
